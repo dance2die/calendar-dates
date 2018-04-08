e0b007182ad87e30af067057c3689e1ace3e7b37
Sung Kim <sungmkim80@gmail.com>
2018-04-07 23:52:16 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# throwaway.CalendarDates
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,69 @@
+const l = console.log;
+
+// if (document.readyState === "complete") {
+//   l("document is already ready, just execute code here");
+//   main();
+// } else {
+//   document.addEventListener("DOMContentLoaded", function() {
+//     l("document was not ready, place code here");
+//     main();
+//   });
+// }
+
+main();
+
+function setInnerContent(content) {
+  document.querySelector("#app").innerHTML = content;
+}
+
+function main() {
+  // setInnerContent("<h1>hi!</h1>");
+  // 8/1/2018
+  const dates = getCalendarDates(new Date(2018, 7, 1));
+}
+
+function getCalendarDates(date) {
+  l(`date=${date}`);
+  const result = [];
+
+  // Get Previous Month's Dates
+  const prevMonthDates = getPreviousMonthDates(date);
+  console.log(`prevMonthDates=${prevMonthDates}`);
+  result.push(prevMonthDates);
+
+  // // Get Current Month's Dates
+  // const currMonthDates = getCurrentMonthDates(month);
+  // result.push(currMonthDates);
+
+  // // Get Next Month's Dates
+  // const nextMonthDates = getNextMonthDates(month);
+  // result.push(nextMonthDates);
+}
+
+function getPreviousMonthDates(date) {
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const prevMonth = Math.min(month - 1, 11);
+  const prevDate = new Date(year, prevMonth);
+
+  const prevMonthLastDate = getLastDate(prevDate);
+  const firstDayIndex = getFirstDayIndex(date);
+  const start = prevMonthLastDate - firstDayIndex + 1;
+  const length = prevMonthLastDate - start + 1;
+
+  return Array(length)
+    .fill()
+    .map((e, i) => start + i);
+}
+
+function getLastDate(date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
+
+function getFirstDayIndex(date) {
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1)
+    .toDateString()
+    .substring(0, 3);
+  return dayNames.indexOf(firstDay);
+}
