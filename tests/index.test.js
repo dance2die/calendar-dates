bfd98fee15964647432cfe98ad96de146583beca
Sung Kim <sungmkim80@gmail.com>
2018-04-18 22:44:05 -0400

    Added tests for "getDateMatrix". Passes like a boss 👍

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,9 +9,37 @@ test("Dates for April of 2018", () => {
   expect(actual).toEqual(expected);
 });
 
+test("Date Matrix for April of 2018", () => {
+  const actual = sut.getDateMatrix(new Date(2018, 3));
+  // prettier-ignore
+  const expected = [ 
+    [ 1, 2, 3, 4, 5, 6, 7 ],
+    [ 8, 9, 10, 11, 12, 13, 14 ],
+    [ 15, 16, 17, 18, 19, 20, 21 ],
+    [ 22, 23, 24, 25, 26, 27, 28 ],
+    [ 29, 30, 1, 2, 3, 4, 5 ],
+    [ 6, 7, 8, 9, 10, 11, 12 ]
+  ];
+  expect(actual).toEqual(expected);
+});
+
 test("Dates for May of 2018", () => {
   const actual = sut.getDates(new Date(2018, 4));
   // prettier-ignore
   const expected = [29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,1,2,3,4,5,6,7,8,9];
+  expect(actual).toEqual(expected);
+});
+
+test("Date Matrix for May of 2018", () => {
+  const actual = sut.getDateMatrix(new Date(2018, 4));
+  // prettier-ignore
+  const expected = [ 
+    [ 29, 30, 1, 2, 3, 4, 5 ],
+    [ 6, 7, 8, 9, 10, 11, 12 ],
+    [ 13, 14, 15, 16, 17, 18, 19 ],
+    [ 20, 21, 22, 23, 24, 25, 26 ],
+    [ 27, 28, 29, 30, 31, 1, 2 ],
+    [ 3, 4, 5, 6, 7, 8, 9 ]
+  ];
   expect(actual).toEqual(expected);
 });
