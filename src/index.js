73d2e4900c3a58ddf603c299e2344baddf9c8971
Sung Kim <sungmkim80@gmail.com>
2018-06-10 11:00:54 -0400

    exposed only 2 methods for version 2

--- src/index.js
+++ src/index.js
@@ -1,123 +1,131 @@
+const monthTypes = {
+  PREVIOUS: "previous",
+  CURRENT: "current",
+  NEXT: "next"
+};
+
 class CalendarDates {
-  static monthTypes = {
-    PREVIOUS: "previous",
-    CURRENT: "current",
-    NEXT: "next"
-  };
-
-  getDateMatrix(date) {
-    const dates = this.getDates(date);
-    const daysInAWeek = 7; // 7 days in a week.
-
-    // https://stackoverflow.com/a/39838921/4035
-    return dates.reduce(
-      (rows, key, index) =>
-        (index % daysInAWeek === 0
-          ? rows.push([key])
-          : rows[rows.length - 1].push(key)) && rows,
-      []
-    );
+  async getDates(date) {
+    return getDatesWithMetadataAsync(date);
   }
 
-  async getDateMatrixAsync(date) {
-    return this.getDateMatrix(date);
+  async getMatrix(date) {
+    return getDateMatrixWithMetadataAsync(date);
   }
+}
 
-  getDateMatrixWithMetadata(date) {
-    const dates = this.getDatesWithMetadata(date);
-    const daysInAWeek = 7; // 7 days in a week.
+function getDateMatrix(date) {
+  const dates = getDates(date);
+  const daysInAWeek = 7; // 7 days in a week.
 
-    // https://stackoverflow.com/a/39838921/4035
-    return dates.reduce(
-      (rows, key, index) =>
-        (index % daysInAWeek === 0
-          ? rows.push([key])
-          : rows[rows.length - 1].push(key)) && rows,
-      []
-    );
-  }
+  // https://stackoverflow.com/a/39838921/4035
+  return dates.reduce(
+    (rows, key, index) =>
+      (index % daysInAWeek === 0
+        ? rows.push([key])
+        : rows[rows.length - 1].push(key)) && rows,
+    []
+  );
+}
 
-  async getDateMatrixWithMetadataAsync(date) {
-    return this.getDateMatrixWithMetadata(date);
-  }
+async function getDateMatrixAsync(date) {
+  return getDateMatrix(date);
+}
 
-  getDates(date) {
-    return this.getDatesWithMetadata(date).map(metadata => metadata.date);
-  }
+function getDateMatrixWithMetadata(date) {
+  const dates = getDatesWithMetadata(date);
+  const daysInAWeek = 7; // 7 days in a week.
 
-  async getDatesAsync(date) {
-    return this.getDates(date);
-  }
+  // https://stackoverflow.com/a/39838921/4035
+  return dates.reduce(
+    (rows, key, index) =>
+      (index % daysInAWeek === 0
+        ? rows.push([key])
+        : rows[rows.length - 1].push(key)) && rows,
+    []
+  );
+}
 
-  getDatesWithMetadata(date) {
-    let result = [];
+async function getDateMatrixWithMetadataAsync(date) {
+  return getDateMatrixWithMetadata(date);
+}
 
-    const prevMonthDates = this.getPreviousDates(date).map(date => ({
-      date,
-      type: CalendarDates.monthTypes.PREVIOUS
-    }));
-    const currMonthDates = this.getCurrentDates(date).map(date => ({
-      date,
-      type: CalendarDates.monthTypes.CURRENT
-    }));
-    result = result.concat(prevMonthDates).concat(currMonthDates);
+function getDates(date) {
+  return getDatesWithMetadata(date).map(metadata => metadata.date);
+}
 
-    const nextMonthDates = this.getNextDates(date, result.length).map(date => ({
-      date,
-      type: CalendarDates.monthTypes.NEXT
-    }));
-    result = result.concat(nextMonthDates);
+async function getDatesAsync(date) {
+  return getDates(date);
+}
 
-    return result;
-  }
+function getDatesWithMetadata(date) {
+  let result = [];
 
-  async getDatesWithMetadataAsync(date) {
-    return this.getDatesWithMetadata(date);
-  }
+  const prevMonthDates = getPreviousDates(date).map(date => ({
+    date,
+    type: monthTypes.PREVIOUS
+  }));
+  const currMonthDates = getCurrentDates(date).map(date => ({
+    date,
+    type: monthTypes.CURRENT
+  }));
+  result = result.concat(prevMonthDates).concat(currMonthDates);
 
-  getCurrentDates(date) {
-    const lastDate = getLastDate(date);
-    return Array(lastDate)
-      .fill()
-      .map((_, i) => i + 1);
-  }
+  const nextMonthDates = getNextDates(date, result.length).map(date => ({
+    date,
+    type: monthTypes.NEXT
+  }));
+  result = result.concat(nextMonthDates);
 
-  async getCurrentDatesAsync(date) {
-    return this.getCurrentDates(date);
-  }
+  return result;
+}
 
-  getPreviousDates(date) {
-    const month = date.getMonth();
-    const year = date.getFullYear();
-    const prevMonth = Math.min(month - 1, 11);
-    const prevDate = new Date(year, prevMonth);
+async function getDatesWithMetadataAsync(date) {
+  return getDatesWithMetadata(date);
+}
 
-    const prevMonthLastDate = getLastDate(prevDate);
-    const firstDayIndex = getFirstDayIndex(date);
-    const start = prevMonthLastDate - firstDayIndex + 1;
-    const length = prevMonthLastDate - start + 1;
+function getCurrentDates(date) {
+  const lastDate = getLastDate(date);
+  return Array(lastDate)
+    .fill()
+    .map((_, i) => i + 1);
+}
 
-    return Array(length)
-      .fill()
-      .map((_, i) => start + i);
-  }
+async function getCurrentDatesAsync(date) {
+  return getCurrentDates(date);
+}
 
-  async getPreviousDatesAsync(date) {
-    return this.getPreviousDates(date);
-  }
+function getPreviousDates(date) {
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const prevMonth = Math.min(month - 1, 11);
+  const prevDate = new Date(year, prevMonth);
 
-  getNextDates(date, daysSoFar) {
-    // 7 days * 6 rows (in a calendar)
-    const totalDays = 42; // not the answer to all questions.
-    const length = totalDays - daysSoFar;
-    return Array(length)
-      .fill()
-      .map((_, i) => i + 1);
-  }
+  const prevMonthLastDate = getLastDate(prevDate);
+  const firstDayIndex = getFirstDayIndex(date);
+  const start = prevMonthLastDate - firstDayIndex + 1;
+  const length = prevMonthLastDate - start + 1;
 
-  async getNextDatesAsync(date, daysSoFar) {
-    return this.getNextDates(date, daysSoFar);
-  }
+  return Array(length)
+    .fill()
+    .map((_, i) => start + i);
+}
+
+async function getPreviousDatesAsync(date) {
+  return getPreviousDates(date);
+}
+
+function getNextDates(date, daysSoFar) {
+  // 7 days * 6 rows (in a calendar)
+  const totalDays = 42; // not the answer to all questions.
+  const length = totalDays - daysSoFar;
+  return Array(length)
+    .fill()
+    .map((_, i) => i + 1);
+}
+
+async function getNextDatesAsync(date, daysSoFar) {
+  return getNextDates(date, daysSoFar);
 }
 
 function getLastDate(date) {
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,37 +1,11 @@
 const CalendarDates = require("../dist/calendardates.cjs");
-// import CalendarDates from "../src/index";
 // "sut" means "System Under Test".
 const sut = new CalendarDates();
 
 const april2018 = new Date(2018, 3);
 const may2018 = new Date(2018, 4);
 
-test("Dates for April of 2018", () => {
-  const actual = sut.getDates(april2018);
-  // prettier-ignore
-  const expected = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12];
-  expect(actual).toEqual(expected);
-
-  // https://facebook.github.io/jest/docs/en/asynchronous.html#promises
-  return sut
-    .getDatesAsync(april2018)
-    .then(actual => expect(actual).toEqual(expected));
-});
-
-test("Dates for May of 2018", () => {
-  const actual = sut.getDates(may2018);
-  // prettier-ignore
-  const expected = [29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,1,2,3,4,5,6,7,8,9];
-  expect(actual).toEqual(expected);
-
-  // https://facebook.github.io/jest/docs/en/asynchronous.html#promises
-  return sut
-    .getDatesAsync(may2018)
-    .then(actual => expect(actual).toEqual(expected));
-});
-
 test("Dates with Metadata for April of 2018", () => {
-  const actual = sut.getDatesWithMetadata(april2018);
   // prettier-ignore
   const expected = [
     {date: 1, type: 'current'}, {date: 2, type: 'current'}, {date: 3, type: 'current'}, {date: 4, type: 'current'}, {date: 5, type: 'current'}, {date: 6, type: 'current'}, {date: 7, type: 'current'}, 
@@ -40,15 +14,13 @@ test("Dates with Metadata for April of 2018", () => {
     {date: 22, type: 'current'}, {date: 23, type: 'current'}, {date: 24, type: 'current'}, {date: 25, type: 'current'}, {date: 26, type: 'current'}, {date: 27, type: 'current'}, {date: 28, type: 'current'}, 
     {date: 29, type: 'current'}, {date: 30, type: 'current'}, {date: 1, type: 'next'}, {date: 2, type: 'next'}, {date: 3, type: 'next'}, {date: 4, type: 'next'}, {date: 5, type: 'next'}, 
     {date: 6, type: 'next'}, {date: 7, type: 'next'}, {date: 8, type: 'next'}, {date: 9, type: 'next'}, {date: 10, type: 'next'}, {date: 11, type: 'next'}, {date: 12, type: 'next'}];
-  expect(actual).toEqual(expected);
 
   return sut
-    .getDatesWithMetadataAsync(april2018)
+    .getDates(april2018)
     .then(actual => expect(actual).toEqual(expected));
 });
 
 test("Dates with Metadata for May of 2018", () => {
-  const actual = sut.getDatesWithMetadata(may2018);
   // prettier-ignore
   const expected = [
     { date: 29, type: "previous" },{ date: 30, type: "previous" },{ date: 1, type: "current" },{ date: 2, type: "current" },{ date: 3, type: "current" },{ date: 4, type: "current" },{ date: 5, type: "current" },
@@ -58,55 +30,11 @@ test("Dates with Metadata for May of 2018", () => {
     { date: 27, type: "current" },{ date: 28, type: "current" },{ date: 29, type: "current" },{ date: 30, type: "current" },{ date: 31, type: "current" },{ date: 1, type: "next" },{ date: 2, type: "next" },
     { date: 3, type: "next" },{ date: 4, type: "next" },{ date: 5, type: "next" },{ date: 6, type: "next" },{ date: 7, type: "next" },{ date: 8, type: "next" },{ date: 9, type: "next" }
   ];
-  expect(actual).toEqual(expected);
 
-  return sut
-    .getDatesWithMetadataAsync(may2018)
-    .then(actual => expect(actual).toEqual(expected));
-});
-
-test("Date Matrix for April of 2018", () => {
-  const actual = sut.getDateMatrix(april2018);
-  // prettier-ignore
-  const expected = [ 
-    [ 1, 2, 3, 4, 5, 6, 7 ],
-    [ 8, 9, 10, 11, 12, 13, 14 ],
-    [ 15, 16, 17, 18, 19, 20, 21 ],
-    [ 22, 23, 24, 25, 26, 27, 28 ],
-    [ 29, 30, 1, 2, 3, 4, 5 ],
-    [ 6, 7, 8, 9, 10, 11, 12 ]
-  ];
-
-  expect(actual).toEqual(expected);
-
-  // https://facebook.github.io/jest/docs/en/asynchronous.html#promises
-  return sut
-    .getDateMatrixAsync(april2018)
-    .then(actual => expect(actual).toEqual(expected));
-});
-
-test("Date Matrix for May of 2018", () => {
-  const actual = sut.getDateMatrix(may2018);
-  // prettier-ignore
-  const expected = [ 
-    [ 29, 30, 1, 2, 3, 4, 5 ],
-    [ 6, 7, 8, 9, 10, 11, 12 ],
-    [ 13, 14, 15, 16, 17, 18, 19 ],
-    [ 20, 21, 22, 23, 24, 25, 26 ],
-    [ 27, 28, 29, 30, 31, 1, 2 ],
-    [ 3, 4, 5, 6, 7, 8, 9 ]
-  ];
-
-  expect(actual).toEqual(expected);
-
-  // https://facebook.github.io/jest/docs/en/asynchronous.html#promises
-  return sut
-    .getDateMatrixAsync(may2018)
-    .then(actual => expect(actual).toEqual(expected));
+  return sut.getDates(may2018).then(actual => expect(actual).toEqual(expected));
 });
 
 test("Date Matrix with Metadata for April of 2018", () => {
-  const actual = sut.getDateMatrixWithMetadata(april2018);
   // prettier-ignore
   const expected = [
     [{date: 1, type: 'current'}, {date: 2, type: 'current'}, {date: 3, type: 'current'}, {date: 4, type: 'current'}, {date: 5, type: 'current'}, {date: 6, type: 'current'}, {date: 7, type: 'current'}], 
@@ -117,15 +45,12 @@ test("Date Matrix with Metadata for April of 2018", () => {
     [{date: 6, type: 'next'}, {date: 7, type: 'next'}, {date: 8, type: 'next'}, {date: 9, type: 'next'}, {date: 10, type: 'next'}, {date: 11, type: 'next'}, {date: 12, type: 'next'}]
   ];
 
-  expect(actual).toEqual(expected);
-
   return sut
-    .getDateMatrixWithMetadataAsync(april2018)
+    .getMatrix(april2018)
     .then(actual => expect(actual).toEqual(expected));
 });
 
 test("Date Matrix with Metadata for May of 2018", () => {
-  const actual = sut.getDateMatrixWithMetadata(may2018);
   // prettier-ignore
   const expected = [
     [{ date: 29, type: "previous" },{ date: 30, type: "previous" },{ date: 1, type: "current" },{ date: 2, type: "current" },{ date: 3, type: "current" },{ date: 4, type: "current" },{ date: 5, type: "current" }],
@@ -136,9 +61,7 @@ test("Date Matrix with Metadata for May of 2018", () => {
     [{ date: 3, type: "next" },{ date: 4, type: "next" },{ date: 5, type: "next" },{ date: 6, type: "next" },{ date: 7, type: "next" },{ date: 8, type: "next" },{ date: 9, type: "next" }]
   ];
 
-  expect(actual).toEqual(expected);
-
   return sut
-    .getDateMatrixWithMetadataAsync(may2018)
+    .getMatrix(may2018)
     .then(actual => expect(actual).toEqual(expected));
 });
