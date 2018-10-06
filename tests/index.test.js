7ac7e155f84f123888760502dd191c5461b72cba
ewolfe <e@ewolfe.me>
2018-10-06 16:39:10 -0700

    Update tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,14 +6,50 @@ const april2018 = new Date(2018, 3);
 const may2018 = new Date(2018, 4);
 
 test("Dates with Metadata for April of 2018", () => {
-  // prettier-ignore
   const expected = [
-    {date: 1, type: 'current'}, {date: 2, type: 'current'}, {date: 3, type: 'current'}, {date: 4, type: 'current'}, {date: 5, type: 'current'}, {date: 6, type: 'current'}, {date: 7, type: 'current'}, 
-    {date: 8, type: 'current'}, {date: 9, type: 'current'}, {date: 10, type: 'current'}, {date: 11, type: 'current'}, {date: 12, type: 'current'}, {date: 13, type: 'current'}, {date: 14, type: 'current'}, 
-    {date: 15, type: 'current'}, {date: 16, type: 'current'}, {date: 17, type: 'current'}, {date: 18, type: 'current'}, {date: 19, type: 'current'}, {date: 20, type: 'current'}, {date: 21, type: 'current'}, 
-    {date: 22, type: 'current'}, {date: 23, type: 'current'}, {date: 24, type: 'current'}, {date: 25, type: 'current'}, {date: 26, type: 'current'}, {date: 27, type: 'current'}, {date: 28, type: 'current'}, 
-    {date: 29, type: 'current'}, {date: 30, type: 'current'}, {date: 1, type: 'next'}, {date: 2, type: 'next'}, {date: 3, type: 'next'}, {date: 4, type: 'next'}, {date: 5, type: 'next'}, 
-    {date: 6, type: 'next'}, {date: 7, type: 'next'}, {date: 8, type: 'next'}, {date: 9, type: 'next'}, {date: 10, type: 'next'}, {date: 11, type: 'next'}, {date: 12, type: 'next'}];
+    { date: 1, iso: "2018-04-01", type: "current" },
+    { date: 2, iso: "2018-04-01", type: "current" },
+    { date: 3, iso: "2018-04-01", type: "current" },
+    { date: 4, iso: "2018-04-01", type: "current" },
+    { date: 5, iso: "2018-04-01", type: "current" },
+    { date: 6, iso: "2018-04-01", type: "current" },
+    { date: 7, iso: "2018-04-01", type: "current" },
+    { date: 8, iso: "2018-04-01", type: "current" },
+    { date: 9, iso: "2018-04-01", type: "current" },
+    { date: 10, iso: "2018-04-01", type: "current" },
+    { date: 11, iso: "2018-04-01", type: "current" },
+    { date: 12, iso: "2018-04-01", type: "current" },
+    { date: 13, iso: "2018-04-01", type: "current" },
+    { date: 14, iso: "2018-04-01", type: "current" },
+    { date: 15, iso: "2018-04-01", type: "current" },
+    { date: 16, iso: "2018-04-01", type: "current" },
+    { date: 17, iso: "2018-04-01", type: "current" },
+    { date: 18, iso: "2018-04-01", type: "current" },
+    { date: 19, iso: "2018-04-01", type: "current" },
+    { date: 20, iso: "2018-04-01", type: "current" },
+    { date: 21, iso: "2018-04-01", type: "current" },
+    { date: 22, iso: "2018-04-01", type: "current" },
+    { date: 23, iso: "2018-04-01", type: "current" },
+    { date: 24, iso: "2018-04-01", type: "current" },
+    { date: 25, iso: "2018-04-01", type: "current" },
+    { date: 26, iso: "2018-04-01", type: "current" },
+    { date: 27, iso: "2018-04-01", type: "current" },
+    { date: 28, iso: "2018-04-01", type: "current" },
+    { date: 29, iso: "2018-04-01", type: "current" },
+    { date: 30, iso: "2018-04-01", type: "current" },
+    { date: 1, iso: "2018-04-01", type: "next" },
+    { date: 2, iso: "2018-04-01", type: "next" },
+    { date: 3, iso: "2018-04-01", type: "next" },
+    { date: 4, iso: "2018-04-01", type: "next" },
+    { date: 5, iso: "2018-04-01", type: "next" },
+    { date: 6, iso: "2018-04-01", type: "next" },
+    { date: 7, iso: "2018-04-01", type: "next" },
+    { date: 8, iso: "2018-04-01", type: "next" },
+    { date: 9, iso: "2018-04-01", type: "next" },
+    { date: 10, iso: "2018-04-01", type: "next" },
+    { date: 11, iso: "2018-04-01", type: "next" },
+    { date: 12, iso: "2018-04-01", type: "next" }
+  ];
 
   return sut
     .getDates(april2018)
@@ -21,28 +57,110 @@ test("Dates with Metadata for April of 2018", () => {
 });
 
 test("Dates with Metadata for May of 2018", () => {
-  // prettier-ignore
   const expected = [
-    { date: 29, type: "previous" },{ date: 30, type: "previous" },{ date: 1, type: "current" },{ date: 2, type: "current" },{ date: 3, type: "current" },{ date: 4, type: "current" },{ date: 5, type: "current" },
-    { date: 6, type: "current" },{ date: 7, type: "current" },{ date: 8, type: "current" },{ date: 9, type: "current" },{ date: 10, type: "current" },{ date: 11, type: "current" },{ date: 12, type: "current" },
-    { date: 13, type: "current" },{ date: 14, type: "current" },{ date: 15, type: "current" },{ date: 16, type: "current" },{ date: 17, type: "current" },{ date: 18, type: "current" },{ date: 19, type: "current" },
-    { date: 20, type: "current" },{ date: 21, type: "current" },{ date: 22, type: "current" },{ date: 23, type: "current" },{ date: 24, type: "current" },{ date: 25, type: "current" },{ date: 26, type: "current" },
-    { date: 27, type: "current" },{ date: 28, type: "current" },{ date: 29, type: "current" },{ date: 30, type: "current" },{ date: 31, type: "current" },{ date: 1, type: "next" },{ date: 2, type: "next" },
-    { date: 3, type: "next" },{ date: 4, type: "next" },{ date: 5, type: "next" },{ date: 6, type: "next" },{ date: 7, type: "next" },{ date: 8, type: "next" },{ date: 9, type: "next" }
+    { date: 29, iso: "2018-05-01", type: "previous" },
+    { date: 30, iso: "2018-05-01", type: "previous" },
+    { date: 1, iso: "2018-05-01", type: "current" },
+    { date: 2, iso: "2018-05-01", type: "current" },
+    { date: 3, iso: "2018-05-01", type: "current" },
+    { date: 4, iso: "2018-05-01", type: "current" },
+    { date: 5, iso: "2018-05-01", type: "current" },
+    { date: 6, iso: "2018-05-01", type: "current" },
+    { date: 7, iso: "2018-05-01", type: "current" },
+    { date: 8, iso: "2018-05-01", type: "current" },
+    { date: 9, iso: "2018-05-01", type: "current" },
+    { date: 10, iso: "2018-05-01", type: "current" },
+    { date: 11, iso: "2018-05-01", type: "current" },
+    { date: 12, iso: "2018-05-01", type: "current" },
+    { date: 13, iso: "2018-05-01", type: "current" },
+    { date: 14, iso: "2018-05-01", type: "current" },
+    { date: 15, iso: "2018-05-01", type: "current" },
+    { date: 16, iso: "2018-05-01", type: "current" },
+    { date: 17, iso: "2018-05-01", type: "current" },
+    { date: 18, iso: "2018-05-01", type: "current" },
+    { date: 19, iso: "2018-05-01", type: "current" },
+    { date: 20, iso: "2018-05-01", type: "current" },
+    { date: 21, iso: "2018-05-01", type: "current" },
+    { date: 22, iso: "2018-05-01", type: "current" },
+    { date: 23, iso: "2018-05-01", type: "current" },
+    { date: 24, iso: "2018-05-01", type: "current" },
+    { date: 25, iso: "2018-05-01", type: "current" },
+    { date: 26, iso: "2018-05-01", type: "current" },
+    { date: 27, iso: "2018-05-01", type: "current" },
+    { date: 28, iso: "2018-05-01", type: "current" },
+    { date: 29, iso: "2018-05-01", type: "current" },
+    { date: 30, iso: "2018-05-01", type: "current" },
+    { date: 31, iso: "2018-05-01", type: "current" },
+    { date: 1, iso: "2018-05-01", type: "next" },
+    { date: 2, iso: "2018-05-01", type: "next" },
+    { date: 3, iso: "2018-05-01", type: "next" },
+    { date: 4, iso: "2018-05-01", type: "next" },
+    { date: 5, iso: "2018-05-01", type: "next" },
+    { date: 6, iso: "2018-05-01", type: "next" },
+    { date: 7, iso: "2018-05-01", type: "next" },
+    { date: 8, iso: "2018-05-01", type: "next" },
+    { date: 9, iso: "2018-05-01", type: "next" }
   ];
 
   return sut.getDates(may2018).then(actual => expect(actual).toEqual(expected));
 });
 
 test("Date Matrix with Metadata for April of 2018", () => {
-  // prettier-ignore
   const expected = [
-    [{date: 1, type: 'current'}, {date: 2, type: 'current'}, {date: 3, type: 'current'}, {date: 4, type: 'current'}, {date: 5, type: 'current'}, {date: 6, type: 'current'}, {date: 7, type: 'current'}], 
-    [{date: 8, type: 'current'}, {date: 9, type: 'current'}, {date: 10, type: 'current'}, {date: 11, type: 'current'}, {date: 12, type: 'current'}, {date: 13, type: 'current'}, {date: 14, type: 'current'}], 
-    [{date: 15, type: 'current'}, {date: 16, type: 'current'}, {date: 17, type: 'current'}, {date: 18, type: 'current'}, {date: 19, type: 'current'}, {date: 20, type: 'current'}, {date: 21, type: 'current'}], 
-    [{date: 22, type: 'current'}, {date: 23, type: 'current'}, {date: 24, type: 'current'}, {date: 25, type: 'current'}, {date: 26, type: 'current'}, {date: 27, type: 'current'}, {date: 28, type: 'current'}], 
-    [{date: 29, type: 'current'}, {date: 30, type: 'current'}, {date: 1, type: 'next'}, {date: 2, type: 'next'}, {date: 3, type: 'next'}, {date: 4, type: 'next'}, {date: 5, type: 'next'}], 
-    [{date: 6, type: 'next'}, {date: 7, type: 'next'}, {date: 8, type: 'next'}, {date: 9, type: 'next'}, {date: 10, type: 'next'}, {date: 11, type: 'next'}, {date: 12, type: 'next'}]
+    [
+      { date: 1, iso: "2018-04-01", type: "current" },
+      { date: 2, iso: "2018-04-01", type: "current" },
+      { date: 3, iso: "2018-04-01", type: "current" },
+      { date: 4, iso: "2018-04-01", type: "current" },
+      { date: 5, iso: "2018-04-01", type: "current" },
+      { date: 6, iso: "2018-04-01", type: "current" },
+      { date: 7, iso: "2018-04-01", type: "current" }
+    ],
+    [
+      { date: 8, iso: "2018-04-01", type: "current" },
+      { date: 9, iso: "2018-04-01", type: "current" },
+      { date: 10, iso: "2018-04-01", type: "current" },
+      { date: 11, iso: "2018-04-01", type: "current" },
+      { date: 12, iso: "2018-04-01", type: "current" },
+      { date: 13, iso: "2018-04-01", type: "current" },
+      { date: 14, iso: "2018-04-01", type: "current" }
+    ],
+    [
+      { date: 15, iso: "2018-04-01", type: "current" },
+      { date: 16, iso: "2018-04-01", type: "current" },
+      { date: 17, iso: "2018-04-01", type: "current" },
+      { date: 18, iso: "2018-04-01", type: "current" },
+      { date: 19, iso: "2018-04-01", type: "current" },
+      { date: 20, iso: "2018-04-01", type: "current" },
+      { date: 21, iso: "2018-04-01", type: "current" }
+    ],
+    [
+      { date: 22, iso: "2018-04-01", type: "current" },
+      { date: 23, iso: "2018-04-01", type: "current" },
+      { date: 24, iso: "2018-04-01", type: "current" },
+      { date: 25, iso: "2018-04-01", type: "current" },
+      { date: 26, iso: "2018-04-01", type: "current" },
+      { date: 27, iso: "2018-04-01", type: "current" },
+      { date: 28, iso: "2018-04-01", type: "current" }
+    ],
+    [
+      { date: 29, iso: "2018-04-01", type: "current" },
+      { date: 30, iso: "2018-04-01", type: "current" },
+      { date: 1, iso: "2018-04-01", type: "next" },
+      { date: 2, iso: "2018-04-01", type: "next" },
+      { date: 3, iso: "2018-04-01", type: "next" },
+      { date: 4, iso: "2018-04-01", type: "next" },
+      { date: 5, iso: "2018-04-01", type: "next" }
+    ],
+    [
+      { date: 6, iso: "2018-04-01", type: "next" },
+      { date: 7, iso: "2018-04-01", type: "next" },
+      { date: 8, iso: "2018-04-01", type: "next" },
+      { date: 9, iso: "2018-04-01", type: "next" },
+      { date: 10, iso: "2018-04-01", type: "next" },
+      { date: 11, iso: "2018-04-01", type: "next" },
+      { date: 12, iso: "2018-04-01", type: "next" }
+    ]
   ];
 
   return sut
@@ -51,14 +169,61 @@ test("Date Matrix with Metadata for April of 2018", () => {
 });
 
 test("Date Matrix with Metadata for May of 2018", () => {
-  // prettier-ignore
   const expected = [
-    [{ date: 29, type: "previous" },{ date: 30, type: "previous" },{ date: 1, type: "current" },{ date: 2, type: "current" },{ date: 3, type: "current" },{ date: 4, type: "current" },{ date: 5, type: "current" }],
-    [{ date: 6, type: "current" },{ date: 7, type: "current" },{ date: 8, type: "current" },{ date: 9, type: "current" },{ date: 10, type: "current" },{ date: 11, type: "current" },{ date: 12, type: "current" }],
-    [{ date: 13, type: "current" },{ date: 14, type: "current" },{ date: 15, type: "current" },{ date: 16, type: "current" },{ date: 17, type: "current" },{ date: 18, type: "current" },{ date: 19, type: "current" }],
-    [{ date: 20, type: "current" },{ date: 21, type: "current" },{ date: 22, type: "current" },{ date: 23, type: "current" },{ date: 24, type: "current" },{ date: 25, type: "current" },{ date: 26, type: "current" }],
-    [{ date: 27, type: "current" },{ date: 28, type: "current" },{ date: 29, type: "current" },{ date: 30, type: "current" },{ date: 31, type: "current" },{ date: 1, type: "next" },{ date: 2, type: "next" }],
-    [{ date: 3, type: "next" },{ date: 4, type: "next" },{ date: 5, type: "next" },{ date: 6, type: "next" },{ date: 7, type: "next" },{ date: 8, type: "next" },{ date: 9, type: "next" }]
+    [
+      { date: 29, iso: "2018-05-01", type: "previous" },
+      { date: 30, iso: "2018-05-01", type: "previous" },
+      { date: 1, iso: "2018-05-01", type: "current" },
+      { date: 2, iso: "2018-05-01", type: "current" },
+      { date: 3, iso: "2018-05-01", type: "current" },
+      { date: 4, iso: "2018-05-01", type: "current" },
+      { date: 5, iso: "2018-05-01", type: "current" }
+    ],
+    [
+      { date: 6, iso: "2018-05-01", type: "current" },
+      { date: 7, iso: "2018-05-01", type: "current" },
+      { date: 8, iso: "2018-05-01", type: "current" },
+      { date: 9, iso: "2018-05-01", type: "current" },
+      { date: 10, iso: "2018-05-01", type: "current" },
+      { date: 11, iso: "2018-05-01", type: "current" },
+      { date: 12, iso: "2018-05-01", type: "current" }
+    ],
+    [
+      { date: 13, iso: "2018-05-01", type: "current" },
+      { date: 14, iso: "2018-05-01", type: "current" },
+      { date: 15, iso: "2018-05-01", type: "current" },
+      { date: 16, iso: "2018-05-01", type: "current" },
+      { date: 17, iso: "2018-05-01", type: "current" },
+      { date: 18, iso: "2018-05-01", type: "current" },
+      { date: 19, iso: "2018-05-01", type: "current" }
+    ],
+    [
+      { date: 20, iso: "2018-05-01", type: "current" },
+      { date: 21, iso: "2018-05-01", type: "current" },
+      { date: 22, iso: "2018-05-01", type: "current" },
+      { date: 23, iso: "2018-05-01", type: "current" },
+      { date: 24, iso: "2018-05-01", type: "current" },
+      { date: 25, iso: "2018-05-01", type: "current" },
+      { date: 26, iso: "2018-05-01", type: "current" }
+    ],
+    [
+      { date: 27, iso: "2018-05-01", type: "current" },
+      { date: 28, iso: "2018-05-01", type: "current" },
+      { date: 29, iso: "2018-05-01", type: "current" },
+      { date: 30, iso: "2018-05-01", type: "current" },
+      { date: 31, iso: "2018-05-01", type: "current" },
+      { date: 1, iso: "2018-05-01", type: "next" },
+      { date: 2, iso: "2018-05-01", type: "next" }
+    ],
+    [
+      { date: 3, iso: "2018-05-01", type: "next" },
+      { date: 4, iso: "2018-05-01", type: "next" },
+      { date: 5, iso: "2018-05-01", type: "next" },
+      { date: 6, iso: "2018-05-01", type: "next" },
+      { date: 7, iso: "2018-05-01", type: "next" },
+      { date: 8, iso: "2018-05-01", type: "next" },
+      { date: 9, iso: "2018-05-01", type: "next" }
+    ]
   ];
 
   return sut
