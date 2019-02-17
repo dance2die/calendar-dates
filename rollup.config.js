e83c3d01bf94a68bfbe9c0f75cc29fc0b105c5f0
Sung Kim <sungmkim80@gmail.com>
2019-02-17 10:11:06 -0500

    🛠 Updated Babel and other package versions and replaced uglify with terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
-import uglify from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
@@ -23,6 +23,6 @@ export default {
       exclude: "node_modules/**"
     }),
     commonjs(),
-    uglify()
+    terser()
   ]
 };
